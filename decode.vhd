7e0b452db8ee778b26fdf780d2a55767dac78eea
habeel <137081333+habeelali@users.noreply.github.com>
2025-05-17 18:36:20 +0500

    Fix minor issues in decode

--- decode.vhd
+++ decode.vhd
@@ -4,14 +4,14 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity decode is
     port (
-        instruction : in STD_LOGIC_VECTOR (31 downto 0);
-        clock       : in STD_LOGIC;
-        reset       : in STD_LOGIC;
-        RegDst      : in STD_LOGIC;
-        RegWrite    : in STD_LOGIC;
-        MemToReg    : in STD_LOGIC;
-        memory_data : in STD_LOGIC_VECTOR (31 downto 0);
-        alu_result  : in STD_LOGIC_VECTOR (31 downto 0);
+        instruction : in  STD_LOGIC_VECTOR (31 downto 0);
+        clock       : in  STD_LOGIC;
+        reset       : in  STD_LOGIC;
+        RegDst      : in  STD_LOGIC;
+        RegWrite    : in  STD_LOGIC;
+        MemToReg    : in  STD_LOGIC;
+        memory_data : in  STD_LOGIC_VECTOR (31 downto 0);
+        alu_result  : in  STD_LOGIC_VECTOR (31 downto 0);
         register_rs : out STD_LOGIC_VECTOR (31 downto 0);
         register_rt : out STD_LOGIC_VECTOR (31 downto 0);
         immediate   : out STD_LOGIC_VECTOR (31 downto 0);
@@ -21,7 +21,7 @@ end decode;
 
 architecture Behavioral of decode is
     type reg_array is array(0 to 31) of STD_LOGIC_VECTOR(31 downto 0);
-    shared variable RegFile: reg_array := (
+    signal RegFile : reg_array := (
         X"00000000", -- Register 0 (ZERO)
         X"11111111", -- Register 1
         X"22222222", -- Register 2
@@ -29,25 +29,30 @@ architecture Behavioral of decode is
         X"44444444", -- Register 4
         others => X"01010101"
     );
+
 begin
 
     -- Register Write Process (Synchronous)
     reg_writer: process(clock, reset)
         variable write_index : integer;
         variable write_value : STD_LOGIC_VECTOR(31 downto 0);
+        variable temp_reg : reg_array;
     begin
         if reset = '1' then
-            RegFile := (
-                X"00000000", -- Reset Register 0
+            temp_reg := (
+                X"00000000", -- Register 0 (ZERO)
                 X"11111111", -- Register 1
                 X"22222222", -- Register 2
                 X"33333333", -- Register 3
                 X"44444444", -- Register 4
                 others => X"01010101"
             );
+            RegFile <= temp_reg;
         elsif rising_edge(clock) then
+            temp_reg := RegFile; -- Copy current state
+
             if RegWrite = '1' then
-                -- Determine destination register index
+                -- Determine destination register
                 if RegDst = '0' then
                     write_index := to_integer(unsigned(instruction(20 downto 16)));
                 else
@@ -56,20 +61,23 @@ begin
 
                 -- Prevent write to register 0
                 if write_index /= 0 then
-                    -- Select write value source
+                    -- Select data source
                     if MemToReg = '1' then
                         write_value := memory_data;
                     else
                         write_value := alu_result;
                     end if;
-                    RegFile(write_index) := write_value;
+
+                    temp_reg(write_index) := write_value;
                 end if;
             end if;
+
+            RegFile <= temp_reg; -- Update registers
         end if;
     end process;
 
-    -- Register Read Process (Combinational)
-    reg_read: process(instruction)
+    -- Register Read and Immediate Decode (Combinational)
+    reg_read: process(instruction, RegFile)
         variable rs_addr, rt_addr : integer;
         variable imm : STD_LOGIC_VECTOR(31 downto 0);
     begin
@@ -83,15 +91,11 @@ begin
 
         -- Sign-extend immediate
         imm(15 downto 0) := instruction(15 downto 0);
-        if instruction(15) = '1' then
-            imm(31 downto 16) := (others => '1');
-        else
-            imm(31 downto 16) := (others => '0');
-        end if;
+        imm(31 downto 16) := (others => instruction(15));
         immediate <= imm;
 
-        -- Compute jump address
-        jump_addr <= "0000" & instruction(25 downto 0) & "00";
+        -- Compute jump address (shift left by 6 bits)
+        jump_addr <= "000000" & instruction(25 downto 0);
     end process;
 
 end Behavioral;
